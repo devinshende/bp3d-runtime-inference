b77db4a9fc444bf5d503c4e2f41f57e82c6da545
Devin Shende <devin.s.shende@gmail.com>
2023-08-14 14:01:14 -0700

    predictions

--- app.py
+++ app.py
@@ -2,39 +2,63 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 from forms import CourseForm, columns
 from pprint import pprint
 import os
+import numpy as np
+import sklearn
+import pickle
+import humanize
+from datetime import timedelta
 
 app = Flask(__name__)
 secret_key = os.getenv('SECRET_KEY')
 app.config['SECRET_KEY'] = secret_key
 
-courses_list = [{
-	'title': 'Python 101',
-	'description': 'Learn Python basics',
-	'price': 34,
-	'available': True,
-	'level': 'Beginner'
-}]
 
-in_data = {key: None for key in columns}
+form_data = {key: None for key in columns}
+pred_data = None
+
+CATEGORIES = [1934, 3305, 10956, 25866]
+CATEGORY_CONVERSION = ['0-32 min', '33-55 min', '56 min-3hr','3-7 hrs']
+
+def print_categories():
+    global CATEGORIES
+    for i in range(len(CATEGORIES)):
+        print(f"{convert(CATEGORIES[i])}   \t{i}")
+
+def convert(seconds):
+    return humanize.naturaldelta(timedelta(seconds=seconds))
+
+
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
+	global pred_data, form_data
 	form = CourseForm()
 	if form.validate_on_submit():
 		print(form._fields)
-		in_data['area'] 					= float(form.area.data)
-		in_data['canopy_moisture'] 			= float(form.canopy_moisture.data)
-		in_data['run_max_mem_rss_bytes'] 	= float(form.run_max_mem_rss_bytes.data)
-		in_data['sim_time'] 				= float(form.sim_time.data)
-		in_data['steps_fire'] 				= float(form.steps_fire.data)
-		in_data['surface_moisture'] 		= float(form.surface_moisture.data)
-		in_data['timestep'] 				= float(form.timestep.data)
-		in_data['wind_direction'] 			= float(form.wind_direction.data)
-		in_data['wind_speed'] 				= float(form.wind_speed.data)
+		form_data['sim_time'] 				= float(form.sim_time.data)
+		form_data['surface_moisture'] 		= float(form.surface_moisture.data)
+		form_data['timestep'] 				= float(form.timestep.data)
+		form_data['wind_direction'] 		= float(form.wind_direction.data)
+		form_data['wind_speed'] 			= float(form.wind_speed.data)
+		form_data['canopy_moisture'] 		= float(form.canopy_moisture.data)
+		form_data['run_max_mem_rss_bytes'] 	= float(form.run_max_mem_rss_bytes.data)
+		form_data['area'] 					= float(form.area.data)
+		form_data['steps_fire'] 			= float(form.steps_fire.data)
+		pred_data = np.array(list(form_data.values()))
 		return redirect(url_for('courses'))
 	print("errors" ,form.errors)
 	return render_template('index.html', form=form)
 
+# RENAME
 @app.route('/courses/')
 def courses():
-	return render_template('courses.html', in_data=in_data.items())
+	with open('models.pkl','rb') as file:
+		models = pickle.load(file)
+		print(models)
+	print("======PREDICTION======")
+	k = 'GradientBoosting'
+	prediction = models[k].predict([pred_data])[0]
+	print(prediction)
+	print(CATEGORIES[prediction])
+	prediction_str = CATEGORY_CONVERSION[prediction]
+	return render_template('courses.html', form_data=form_data.items(), prediction_str=prediction_str)
--- forms.py
+++ forms.py
@@ -18,14 +18,14 @@ columns = [
 class CourseForm(FlaskForm):
 
 	sim_time 				= IntegerField('sim_time',validators=[InputRequired()], default=1200)
-	surface_moisture 		= DecimalField('surface_moisture',validators=[InputRequired()], default=1.0)
-	timestep 				= IntegerField('timestep',validators=[InputRequired()])
-	wind_direction			= IntegerField('wind_direction',validators=[InputRequired()])
-	wind_speed 				= IntegerField('wind_speed',validators=[InputRequired()])
-	canopy_moisture			= IntegerField('canopy_moisture',validators=[InputRequired()])
-	run_max_mem_rss_bytes	= IntegerField('run_max_mem_rss_bytes',validators=[InputRequired()])
-	area 					= IntegerField('area',validators=[InputRequired()])
-	steps_fire				= IntegerField('steps_fire',validators=[InputRequired()])
+	surface_moisture 		= DecimalField('surface_moisture',validators=[InputRequired()], default=10.2)
+	timestep 				= IntegerField('timestep',validators=[InputRequired()],default=4)
+	wind_direction			= IntegerField('wind_direction',validators=[InputRequired()],default=40)
+	wind_speed 				= IntegerField('wind_speed',validators=[InputRequired()],default=20)
+	canopy_moisture			= IntegerField('canopy_moisture',validators=[InputRequired()],default=1)
+	run_max_mem_rss_bytes	= IntegerField('run_max_mem_rss_bytes',validators=[InputRequired()],default=24)
+	area 					= IntegerField('area',validators=[InputRequired()],default=400)
+	steps_fire				= IntegerField('steps_fire',validators=[InputRequired()],default=100)
 
 	# title = StringField('Title', validators=[InputRequired(), Length(min=10, max=100)])
 	
